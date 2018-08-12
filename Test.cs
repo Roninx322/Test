8c6e81fcc5ae694b603204faf7438acf010fd849
Roninx322 <911qwer911@gmail.com>
2018-08-12 10:16:06 +0300

    init commit

---
+++ Test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Test
+    {
+        public bool Transfer(Account from, Account to, double amount)
+        {
+            double balance = from.Balance - amount;//в данном случае не есть хорошо работать с переданным классом лучше через переменную потому что если сразу вычесть из класса то оно сохранит изменения 
+            if (balance < 0) return false;//думая нельзя что бы был минусавой баланс ))
+            else
+            {
+                from.Balance -= amount;
+                to.Balance += amount;//to.Balance = from.Balance + amount; было явной ошибкой
+                return true;
+            }
+        }
+    }
+}
